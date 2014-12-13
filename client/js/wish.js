32eb7ddaa0b5d25a12b876a5ee767ae0cc39ea2f
Sean Dolphin <Sean.Dolphin@kpcompass.com>
2014-12-13 16:16:17 -0700

    firebase information added to controller

--- client/js/wish.js
+++ client/js/wish.js
@@ -1,112 +1,123 @@
-angular.module( 'sample', [
-  'auth0',
-  'ngRoute',
-  'sample.home',
-  'sample.login',
-  'angular-storage',
-  'angular-jwt',
-  'wish.templates'
-])
-.config( ['$routeProvider', 'authProvider', '$httpProvider', '$locationProvider', 'jwtInterceptorProvider', function myAppConfig ( $routeProvider, authProvider, $httpProvider, $locationProvider,
-  jwtInterceptorProvider) {
-  $routeProvider
-    .when( '/', {
-      controller: 'HomeCtrl',
-      templateUrl: 'home/home.html',
-      pageTitle: 'Homepage',
-      requiresLogin: true
-    })
-    .when( '/login', {
-      controller: 'LoginCtrl',
-      templateUrl: 'login/login.html',
-      pageTitle: 'Login'
+angular.module('sample', [
+    'auth0',
+    'ngRoute',
+    'sample.home',
+    'sample.login',
+    'angular-storage',
+    'angular-jwt',
+    'wish.templates',
+    'firebase'
+  ])
+  .config(['$routeProvider', 'authProvider', '$httpProvider', '$locationProvider', 'jwtInterceptorProvider', function myAppConfig($routeProvider, authProvider, $httpProvider, $locationProvider,
+    jwtInterceptorProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/home.html',
+        pageTitle: 'Homepage',
+        requiresLogin: true
+      })
+      .when('/login', {
+        controller: 'LoginCtrl',
+        templateUrl: 'login/login.html',
+        pageTitle: 'Login'
+      });
+
+
+    authProvider.init({
+      domain: AUTH0_DOMAIN,
+      clientID: AUTH0_CLIENT_ID,
+      loginUrl: '/login'
     });
 
+    jwtInterceptorProvider.tokenGetter = function(store) {
+      return store.get('token');
+    }
 
-  authProvider.init({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
-    loginUrl: '/login'
-  });
-
-  jwtInterceptorProvider.tokenGetter = function(store) {
-    return store.get('token');
-  }
-
-  // Add a simple interceptor that will fetch all requests and add the jwt token to its authorization header.
-  // NOTE: in case you are calling APIs which expect a token signed with a different secret, you might
-  // want to check the delegation-token example
-  $httpProvider.interceptors.push('jwtInterceptor');
-}]).run(['$rootScope', 'auth', 'store', 'jwtHelper', '$location', function($rootScope, auth, store, jwtHelper, $location) {
-  $rootScope.$on('$locationChangeStart', function() {
-    if (!auth.isAuthenticated) {
-      var token = store.get('token');
-      if (token) {
-        if (!jwtHelper.isTokenExpired(token)) {
-          auth.authenticate(store.get('profile'), token);
-        } else {
-          $location.path('/login');
+    // Add a simple interceptor that will fetch all requests and add the jwt token to its authorization header.
+    // NOTE: in case you are calling APIs which expect a token signed with a different secret, you might
+    // want to check the delegation-token example
+    $httpProvider.interceptors.push('jwtInterceptor');
+  }]).run(['$rootScope', 'auth', 'store', 'jwtHelper', '$location', function($rootScope, auth, store, jwtHelper, $location) {
+    $rootScope.$on('$locationChangeStart', function() {
+      if (!auth.isAuthenticated) {
+        var token = store.get('token');
+        if (token) {
+          if (!jwtHelper.isTokenExpired(token)) {
+            auth.authenticate(store.get('profile'), token);
+          } else {
+            $location.path('/login');
+          }
         }
       }
-    }
 
-  });
-}])
-.controller( 'AppCtrl', ['$scope', '$location', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$routeChangeSuccess', function(e, nextRoute){
-    if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
-      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample' ;
-    }
-  });
-}])
+    });
+  }])
+  .controller('AppCtrl', ['$scope', '$location', '$firebase', function AppCtrl($scope, $location, $firebase) {
+    var ref = new Firebase("https://torid-torch-3093.firebaseio.com");
+
+    ref.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        //do something with it
+      })
+      //OR ---
+      // var sync = $firebase(ref);
+      // $scope.data = sync.$asObject();
+
+    $scope.$on('$routeChangeSuccess', function(e, nextRoute) {
+      if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
+        $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample';
+      }
+    });
+  }])
 
 ;
 
 
-var AUTH0_CLIENT_ID='rWkTAoi309hAeOJVcMdCfy2J18oOVR9M'; 
-var AUTH0_DOMAIN='holidayjs-wish-list.auth0.com'; 
-var AUTH0_CALLBACK_URL=location.href;
-angular.module( 'sample.home', [
-'auth0'
-])
-.controller( 'HomeCtrl', ['$scope', 'auth', '$http', '$location', 'store', function HomeController( $scope, auth, $http, $location, store ) {
+var AUTH0_CLIENT_ID = 'rWkTAoi309hAeOJVcMdCfy2J18oOVR9M';
+var AUTH0_DOMAIN = 'holidayjs-wish-list.auth0.com';
+var AUTH0_CALLBACK_URL = location.href;
+angular.module('sample.home', [
+    'auth0'
+  ])
+  .controller('HomeCtrl', ['$scope', 'auth', '$http', '$location', 'store', function HomeController($scope, auth, $http, $location, store) {
 
-  $scope.auth = auth;
+    $scope.auth = auth;
 
-  $scope.callApi = function() {
-    // Just call the API as you'd do using $http
-    $http({
-      url: 'http://localhost:3001/secured/ping',
-      method: 'GET'
-    }).then(function() {
-      alert("We got the secured data successfully");
-    }, function() {
-      alert("Please download the API seed so that you can call it.");
-    });
-  }
+    $scope.callApi = function() {
+      // Just call the API as you'd do using $http
+      $http({
+        url: 'http://localhost:3001/secured/ping',
+        method: 'GET'
+      }).then(function() {
+        alert("We got the secured data successfully");
+      }, function() {
+        alert("Please download the API seed so that you can call it.");
+      });
+    }
 
-  $scope.logout = function() {
-    auth.signout();
-    store.remove('profile');
-    store.remove('token');
-    $location.path('/login');
-  }
+    $scope.logout = function() {
+      auth.signout();
+      store.remove('profile');
+      store.remove('token');
+      $location.path('/login');
+    }
 
-}]);
+  }]);
 
-angular.module( 'sample.login', [
-  'auth0'
-])
-.controller( 'LoginCtrl', ['$scope', 'auth', '$location', 'store', function HomeController( $scope, auth, $location, store ) {
+angular.module('sample.login', [
+    'auth0'
+  ])
+  .controller('LoginCtrl', ['$scope', 'auth', '$location', 'store', function HomeController($scope, auth, $location, store) {
 
-  $scope.login = function() {
-    auth.signin({}, function(profile, token) {
-      store.set('profile', profile);
-      store.set('token', token);
-      $location.path("/");
-    }, function(error) {
-      console.log("There was an error logging in", error);
-    });
-  }
+    $scope.login = function() {
+      auth.signin({}, function(profile, token) {
+        store.set('profile', profile);
+        store.set('token', token);
+        $location.path("/");
+      }, function(error) {
+        console.log("There was an error logging in", error);
+      });
+    }
 
-}]);
+  }]);
