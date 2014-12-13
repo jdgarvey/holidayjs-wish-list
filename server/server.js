6a6f19957ca37b19233c1589a5f2740d32ad5f02
Ben Sparks <bsparks42@gmail.com>
2014-12-13 15:38:24 -0700

    build everything off gulp

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ boot(app, __dirname);
 // passing the static middleware are hitting the file system
 // Example:
    var path = require('path');
-   app.use(loopback.static(path.resolve(__dirname, '../src')));
+   app.use(loopback.static(path.resolve(__dirname, '../client')));
 
 // Requests that get this far won't be handled
 // by any middleware. Convert them into a 404 error
