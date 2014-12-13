3218f2ea42f3f05cd28a91b6ec40509284adff25
Ben Sparks <bsparks42@gmail.com>
2014-12-13 15:08:39 -0700

    working on build, using compass and concat

---
+++ client/js/wish.js
@@ -0,0 +1,111 @@
+angular.module( 'sample', [
+  'auth0',
+  'ngRoute',
+  'sample.home',
+  'sample.login',
+  'angular-storage',
+  'angular-jwt'
+])
+.config( function myAppConfig ( $routeProvider, authProvider, $httpProvider, $locationProvider,
+  jwtInterceptorProvider) {
+  $routeProvider
+    .when( '/', {
+      controller: 'HomeCtrl',
+      templateUrl: 'home/home.html',
+      pageTitle: 'Homepage',
+      requiresLogin: true
+    })
+    .when( '/login', {
+      controller: 'LoginCtrl',
+      templateUrl: 'login/login.html',
+      pageTitle: 'Login'
+    });
+
+
+  authProvider.init({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    loginUrl: '/login'
+  });
+
+  jwtInterceptorProvider.tokenGetter = function(store) {
+    return store.get('token');
+  }
+
+  // Add a simple interceptor that will fetch all requests and add the jwt token to its authorization header.
+  // NOTE: in case you are calling APIs which expect a token signed with a different secret, you might
+  // want to check the delegation-token example
+  $httpProvider.interceptors.push('jwtInterceptor');
+}).run(function($rootScope, auth, store, jwtHelper, $location) {
+  $rootScope.$on('$locationChangeStart', function() {
+    if (!auth.isAuthenticated) {
+      var token = store.get('token');
+      if (token) {
+        if (!jwtHelper.isTokenExpired(token)) {
+          auth.authenticate(store.get('profile'), token);
+        } else {
+          $location.path('/login');
+        }
+      }
+    }
+
+  });
+})
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$routeChangeSuccess', function(e, nextRoute){
+    if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
+      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample' ;
+    }
+  });
+})
+
+;
+
+
+var AUTH0_CLIENT_ID='rWkTAoi309hAeOJVcMdCfy2J18oOVR9M'; 
+var AUTH0_DOMAIN='holidayjs-wish-list.auth0.com'; 
+var AUTH0_CALLBACK_URL=location.href;
+angular.module( 'sample.home', [
+'auth0'
+])
+.controller( 'HomeCtrl', function HomeController( $scope, auth, $http, $location, store ) {
+
+  $scope.auth = auth;
+
+  $scope.callApi = function() {
+    // Just call the API as you'd do using $http
+    $http({
+      url: 'http://localhost:3001/secured/ping',
+      method: 'GET'
+    }).then(function() {
+      alert("We got the secured data successfully");
+    }, function() {
+      alert("Please download the API seed so that you can call it.");
+    });
+  }
+
+  $scope.logout = function() {
+    auth.signout();
+    store.remove('profile');
+    store.remove('token');
+    $location.path('/login');
+  }
+
+});
+
+angular.module( 'sample.login', [
+  'auth0'
+])
+.controller( 'LoginCtrl', function HomeController( $scope, auth, $location, store ) {
+
+  $scope.login = function() {
+    auth.signin({}, function(profile, token) {
+      store.set('profile', profile);
+      store.set('token', token);
+      $location.path("/");
+    }, function(error) {
+      console.log("There was an error logging in", error);
+    });
+  }
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var compass = require('gulp-compass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename')
+    path = require('path');
 
-gulp.task('default', function () {
+gulp.task('compass', function () {
     return gulp.src('src/scss/app.scss')
-        .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
-        .on('error', function (err) { console.log(err.message); })
-        .pipe(gulp.dest('client/css'));
+        .pipe(compass({
+            project: __dirname,
+            css: './client/css',
+            sass: './src/scss'
+        }))
+        .on('error', function (err) {
+            console.log(err.message);
+        })
+        .pipe(gulp.dest('client/css/'));
+});
+
+gulp.task('concat', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(concat('wish.js'))
+        .pipe(gulp.dest('client/js/'))
+        .pipe(rename('wish.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('client/js/'));
 });
